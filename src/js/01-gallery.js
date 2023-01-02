d5f6cc04727a824260a4055fe1cfad9ccc02764d
Svetlana Kulibaba <zmanalova@gmail.com>
2023-01-02 17:30:38 +0200

    .

---
+++ src/js/01-gallery.js
@@ -0,0 +1,23 @@
+// Add imports above this line
+import simpleLightbox from 'simplelightbox';
+// import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// const divGallery = document.querySelector('.gallery');
+// const createGalleryItems = galleryItems
+//   .map(({ preview, original, description }) => {
+//     return `<div class="gallery__item">
+//   <a class="gallery__item" href=${original}>
+//   <img class="gallery__image" src=${preview} alt=${description} />
+// </a>
+// </div>`;
+//   })
+//   .join('');
+// divGallery.insertAdjacentHTML('afterbegin', createGalleryItems);
+
+// var lightbox = new SimpleLightbox('.gallery a', {
+//   captionDelay: 250,
+//   captionsData: 'alt',
+// });
+// Change code below this line
+
+console.log(galleryItems);
